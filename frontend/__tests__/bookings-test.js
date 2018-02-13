6e15b33082912c689e109995b7dabb8583c5d18d
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-02-13 14:58:11 +0000

    Now can create bookings via UI

--- frontend/__tests__/bookings-test.js
+++ frontend/__tests__/bookings-test.js
@@ -5,6 +5,7 @@ import Nav from '../src/components/Nav.jsx';
 import Footer from '../src/components/Footer.jsx';
 import Header from '../src/components/Header.jsx';
 import Form from '../src/components/Form.jsx';
+import nock from 'nock';
 
 test('App component', () => {
     const testBookings = [
@@ -44,7 +45,6 @@ test('Booking component', () => {
     bookingEntry.setState({ booking: testBooking })
 
     expect(bookingEntry).toMatchSnapshot();
-
 });
 
 test('Nav component', () => {
@@ -77,4 +77,37 @@ test('Form component', () => {
     )
 
     expect(form).toMatchSnapshot();
+})
+
+test('Submit booking', (done) => {
+    
+    const expectedPayload = { 
+        "firstname" : "Mark",
+        "lastname" : "Winteringham",
+        "totalprice" : "123",
+        "depositpaid" : "true",
+        "bookingdates" : {
+            "checkin" : "2018-01-01",
+            "checkout" : "2018-01-02"
+        }, "additionalneeds" : "Breakfast"
+    }
+
+    nock('http://localhost')
+      .post('/booking', expectedPayload)
+      .reply(201, () => {
+          done();
+      })
+
+    const form = mount(
+        <Form />
+    )
+
+    form.find('#firstname').instance().value = "Mark"
+    form.find('#lastname').instance().value = "Winteringham"
+    form.find('#totalprice').instance().value = "123"
+    form.find('#depositpaid').instance().value = "true"
+    form.find('#additionalneeds').instance().value = "Breakfast"
+    form.find('#checkin').instance().value = "2018-01-01"
+    form.find('#checkout').instance().value = "2018-01-02"
+    form.find('button').simulate('click');
 })
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
+import request from 'superagent';
 
 export default class Form extends React.Component {
 
-    handleClick(){
-        console.log("This was clicked on");
+    constructor(props){
+        super(props);
+
+        this.updateValue = this.updateValue.bind(this);
+    }
+
+    updateValue(){
+        let bookingModel = {
+            firstname : this.firstNameValue.value,
+            lastname : this.lastNameValue.value,
+            totalprice : this.priceValue.value,
+            depositpaid : this.depositPaidValue.value,
+            bookingdates : {
+                checkin : this.checkInValue.value,
+                checkout : this.checkOutValue.value
+            },
+            additionalneeds : this.additionalNeedsValue.value
+        }
+
+        request
+            .post('/booking')
+            .set('Content-Type', 'application/json')
+            .send(bookingModel)
+            .then((res, err) => {
+                window.location.reload();
+            }).catch(e => {
+                console.log(e);
+            })
     }
 
     render() {
         return (
             <div className="row">
-                <div className="col-md-1"><input type="text" className="form-control input-sm" placeholder="Firstname" /></div>
-                <div className="col-md-2"><input type="text" className="form-control input-sm" placeholder="Lastname" /></div>
-                <div className="col-md-1"><input type="text" className="form-control input-sm" placeholder="Price" /></div>
-                <div className="col-md-1"><input type="text" className="form-control input-sm" placeholder="Deposit?" /></div>
-                <div className="col-md-2"><input type="text" className="form-control input-sm" placeholder="Additional" /></div>
-                <div className="col-md-2"><input type="text" className="form-control input-sm" placeholder="Checkin" /></div>
-                <div className="col-md-2"><input type="text" className="form-control input-sm" placeholder="Checkout" /></div>
-                <div className="col-md-1"><button onClick={this.handleClick} className="btn btn-sm">Add</button></div>
+                <div className="col-md-1"><input ref={el => this.firstNameValue=el} type="text" id="firstname" className="form-control input-sm" placeholder="Firstname" /></div>
+                <div className="col-md-2"><input ref={el => this.lastNameValue=el} type="text" id="lastname" className="form-control input-sm" placeholder="Lastname" /></div>
+                <div className="col-md-1"><input ref={el => this.priceValue=el} type="text" id="totalprice" className="form-control input-sm" placeholder="Price" /></div>
+                <div className="col-md-1"><input ref={el => this.depositPaidValue=el} type="text" id="depositpaid" className="form-control input-sm" placeholder="Deposit?" /></div>
+                <div className="col-md-2"><input ref={el => this.additionalNeedsValue=el} type="text" id="additionalneeds" className="form-control input-sm" placeholder="Additional" /></div>
+                <div className="col-md-2"><input ref={el => this.checkInValue=el} type="text" id="checkin" className="form-control input-sm" placeholder="Checkin" /></div>
+                <div className="col-md-2"><input ref={el => this.checkOutValue=el} type="text" id="checkout" className="form-control input-sm" placeholder="Checkout" /></div>
+                <div className="col-md-1"><button onClick={this.updateValue} className="btn btn-sm">Add</button></div>
             </div>
         )
     }
