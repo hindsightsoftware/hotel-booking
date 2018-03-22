9b2d4956e9b06bb97c36d81685780359777426f0
Alan Parkinson <alan.parkinson@hindsightsoftware.co.uk>
2018-03-22 22:10:26 +0100

    Use a system installed ChromeDriver

--- backend/src/test/java/outside/ui/driverfactory/DriverFactory.java
+++ backend/src/test/java/outside/ui/driverfactory/DriverFactory.java
@@ -9,17 +9,6 @@ import java.net.URL;
 public class DriverFactory {
 
     public WebDriver create(){
-        String filePath = new File("").getAbsolutePath();
-        String pathToChromeDriver = "";
-
-        if(filePath.contains("backend")){
-            pathToChromeDriver = filePath.concat("/src/test/java/outside/ui/driverfactory/chromedriver");
-        } else {
-            pathToChromeDriver = filePath.concat("/backend/src/test/java/outside/ui/driverfactory/chromedriver");
-        }
-
-        System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
-
         return new ChromeDriver();
     }
 }
